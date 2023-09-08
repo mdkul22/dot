29a06dc5d6a861013cd72007116d79c37de83100
Mayunk Kulkarni <mdkul22@gmail.com>
2023-09-08 12:03:34 -0700

    changes to cmp config and corrections to alpha

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -19,11 +19,10 @@ return {
 				"		██║    ╚████║ ╚██████╔╝ ╚██████╔╝   ╚████╝   ██║ ██║ ╚═╝ ██║		",
 				"		╚═╝		╚═══╝  ╚═════╝   ╚═════╝     ╚══╝    ╚═╝ ╚═╝     ╚═╝		",
 			}
-			dashboard.section.header.size = 200
 			-- Set menu
 				dashboard.section.buttons.val = {
 				dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-				dashboard.button( "f", "  > Find file", ":Telescope find_files"),
+				dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
 				dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
 				dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
 				dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
