85fdd5aba14b61a7f03be7cd8642aab8a2166ae1
Mayunk Kulkarni <mdkul22@gmail.com>
2023-09-08 10:51:58 -0700

    custom start pane

---
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -0,0 +1,48 @@
+return {
+	{
+		'goolord/alpha-nvim',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons',
+		},
+		config = function ()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
+			-- Set header
+			dashboard.section.header.val = {
+				"		███╗      ██╗ ██╗   ██╗ ██╗   ██╗ ██╗    ██╗ ██╗ ███╗   ███╗		",
+				"		████╗     ██║ ██║   ██║ ██║   ██║ ██║    ██║ ██║ ████╗ ████║		",
+				"		██╔██╗    ██║ ██║   ██║ ██║   ██║ ██║    ██║ ██║ ██╔████╔██║		",
+				"		██║╚██╗   ██║ ██║   ██║ ██║   ██║ ██║    ██║ ██║ ██╔████╔██║		",
+				"		██║ ╚██╗  ██║ ██║   ██║ ██║   ██║ ██║    ██║ ██║ ██╔████╔██║		",
+				"		██║  ╚██╗ ██║ ██║   ██║ ██║   ██║ ╚██╗  ██╔╝ ██║ ██║╚██╔╝██║		",
+				"		██║   ╚██╗██║ ██║   ██║ ██║   ██║  ╚██╗██╔╝  ██║ ██║╚██╔╝██║		",
+				"		██║    ╚████║ ╚██████╔╝ ╚██████╔╝   ╚████╝   ██║ ██║ ╚═╝ ██║		",
+				"		╚═╝		╚═══╝  ╚═════╝   ╚═════╝     ╚══╝    ╚═╝ ╚═╝     ╚═╝		",
+			}
+			dashboard.section.header.size = 200
+			-- Set menu
+				dashboard.section.buttons.val = {
+				dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+				dashboard.button( "f", "  > Find file", ":Telescope find_files"),
+				dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+				dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+				dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+
+			}
+			-- require'alpha'.setup(require'alpha.themes.startify'.config)
+			--dashboard.section.footer.val = {
+			--	"	██████╗  ██████╗  ██████╗ ███╗   ███╗	",
+			--	"	██╔══██╗██╔═══██╗██╔═══██╗████╗ ████║	",
+			--	"	██║  ██║██║   ██║██║   ██║██╔████╔██║	",
+			--	"	██║  ██║██║   ██║██║   ██║██║╚██╔╝██║	",
+			--	"	██████╔╝╚██████╔╝╚██████╔╝██║ ╚═╝ ██║	",
+			--	"	╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═	",
+			--}
+			alpha.setup(dashboard.opts)
+			-- Disable folding on alpha buffer
+			vim.cmd([[
+				autocmd FileType alpha setlocal nofoldenable
+			]])
+		end
+	},
+}
