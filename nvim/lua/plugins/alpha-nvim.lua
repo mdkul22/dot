5edc159042511e665766c62b4fd36570bc4223ee
Mayunk Kulkarni <mdkul22@gmail.com>
2023-09-08 17:08:30 -0700

    further changes

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -22,7 +22,8 @@ return {
 			-- Set menu
 				dashboard.section.buttons.val = {
 				dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-				dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
+				dashboard.button( "o", "  > Find old files", ":Telescope oldfiles<CR>"),
+				dashboard.button( "f", "  > Find old files", ":Telescope find_files<CR>"),
 				dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
 				dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
 				dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
