3f5b9881132d2347bafd3c68ea05de4343cdb566
Mayunk Kulkarni <mdkul22@gmail.com>
2023-09-09 21:14:46 -0700

    comments

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -2,6 +2,8 @@
 return {
 
 	-- Better buffer closing actions. Available via the buffers helper.
+    -- for better tag management for projects
     'ludovicchabant/vim-gutentags',
+    -- provides better statusline messages
 	'ojroques/nvim-hardline',
 }
