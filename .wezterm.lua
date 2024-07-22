4713d20e058654226674953209fb4d0600d3e2c2
Mayunk Kulkarni <mdkul22@gmail.com>
2024-07-21 23:07:49 -0700

    everforest update in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Everblush (Gogh)'
+config.color_scheme = 'EverforestDark (Gogh)'
 config.font = wezterm.font_with_fallback {
     '0xProto',
     'Hack'
