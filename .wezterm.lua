2f351ee39b18fe13546011916c3b5515292c86f7
Mayunk Kulkarni <mdkul22@users.noreply.github.com>
2025-06-27 10:29:32 -0700

    Update .wezterm.lua to handle mouse events

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
+local act = wezterm.action
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
@@ -13,7 +13,44 @@ config.font = wezterm.font_with_fallback {
     'Hack'
 }
 config.window_close_confirmation = 'NeverPrompt'
-config.font_size = 18.0
+config.font_size = 16.0
+config.mouse_bindings = {
+  -- Right click sends "woot" to the terminal
+  {
+    event = { Down = { streak = 1, button = 'Right' } },
+    mods = 'NONE',
+    action = act.SendString 'woot',
+  },
 
+  -- Change the default click behavior so that it only selects
+  -- text and doesn't open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+  },
+
+  -- and make CTRL-Click open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  },
+      -- Scrolling up while holding CTRL increases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CTRL',
+    action = act.IncreaseFontSize,
+  },
+
+  -- Scrolling down while holding CTRL decreases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CTRL',
+    action = act.DecreaseFontSize,
+  },
+  -- NOTE that binding only the 'Up' event can give unexpected behaviors.
+  -- Read more below on the gotcha of binding an 'Up' event only.
+}
 -- and finally, return the configuration to wezterm
 return config
